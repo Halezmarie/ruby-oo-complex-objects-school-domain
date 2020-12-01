1eac0229b13d524e5d117540ec2acd4d315604ab
Halezmarie <teacherhaleymarie@gmail.com>
2020-11-30 21:32:31 -0600

    commit tree 49e369457ad54c4c77515b506c9fdc3b4572bc3b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+class School
+
+attr_reader :name, :roster
+#so that nobody else can change the names or roster. 
+
+def initialize(name)
+#like a factory
+    @name = name
+    @roster = Hash.new {|h, k| h[k] = []}
+    #Hash.new {|hash, key| hash[key] = [] }
+    #It allows you to define default value as an array
+end
+
+def roster
+    @roster 
+end
+#allows me to call on the roster after creating it 
+
+def add_student(name,grade)
+self.roster[grade] << name 
+end
+#pass names
+
+
+def grade(grade)
+    self.roster[grade]
+end
+#assign grades
+
+def sort
+    self.roster.each do |grade, students|
+        students.sort!
+        #sort students by grade
+    end
+end
+end
+
